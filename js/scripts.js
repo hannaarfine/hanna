f7672096d146caf64f4841aa2ce1dc41295430bc
Hanna Arfine <ha264@cornell.edu>
2017-10-12 22:32:57 -0400

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
-    $("#demo").fadeIn();
+  $(document).ready(function () {
+    $('.hidden').fadeIn(1500).removeClass('hidden');
+
+    $("#design, #dog").hide();
+  var mentCounter = 0;
+  window.setInterval(function(){
+    if(mentCounter === 0){
+      $("#front").fadeOut();
+      setTimeout(function(){
+        $("#design").fadeIn();
+      },600);
+    }
+    else if(mentCounter == 1){
+      $("#design").fadeOut();
+      setTimeout(function(){
+        $("#dog").fadeIn();
+      },600);
+    }
+    else if(mentCounter == 2){
+      $("#dog").fadeOut();
+      setTimeout(function(){
+        $("#front").fadeIn();
+      },600);
+      mentCounter = -1;
+    }
+    mentCounter++;
+  }, 3500);
+  });
 });
