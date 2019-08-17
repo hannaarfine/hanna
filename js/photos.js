e669267173b498ce45270de23cd26380ade2b5a2
Hanna Arfine <ha264@cornell.edu>
2019-08-17 17:29:45 -0400

    photos

--- js/photos.js
+++ js/photos.js
@@ -43,6 +43,13 @@ var all_entries = {
                   },
                   {'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5cfcdd692427275d062518a2/1136x1515/vsco5cfcdd6c807dc.jpg',
                   'class': 'img-half'
+                  },
+                  {'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d5376cd2fb83812b25c4903/960x1434/vsco5d5376d4a445d.jpg',
+                  'class': 'img-half'
+                  },
+                  {
+                    'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d5376df2fb83812b25c4904/960x1434/vsco5d5376e671068.jpg',
+                    'class': 'img-half'
                   }
               ]
     },
@@ -62,6 +69,18 @@ var all_entries = {
                   'class': 'img-half' 
                   },
                   {
+                    'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d06c41bb60b2e674f33431a/vsco5d06c41ff1a92.jpg',
+                    'class': 'img-half'
+                  },
+                  {
+                    'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d06c400b60b2e674f334319/vsco5d06c4045e74c.jpg',
+                    'class': 'img-half'
+                  },
+                  {
+                    'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d06ca688ea1d2689f856939/vsco5d06ca6be175c.jpg',
+                    'class': 'img-full'
+                  },
+                  {
                     'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d0c87aa19499c611507535e/1136x1515/vsco5d0c87bd69fd2.jpg',
                     'class': 'img-half'
                   },
@@ -72,4 +91,75 @@ var all_entries = {
                   
               ]
     },
+    'italy':{
+        'title': 'Northern Italy',
+        'desc' : 'Road tripping Milan to Como to Florence with some places in between',
+        'date': '06-2019',
+        'cover': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d53764f2fb83812b25c4900/vsco5d53765ddd9c8.jpg',
+        'photos': [
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d18eca6124a810ecf6974f9/960x1434/vsco5d18ecafe54cb.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d18e95e124a810ecf6974ef/960x1434/vsco5d18e95fcd4c5.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d4c674b4cf98a42dd2fea7c/vsco5d4c674d4e927.jpg',
+            'class': 'img-full'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d027d7c98877c4e8a748888/1136x1467/vsco5d027d808e477.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d02afb66e055e5bd9ebb72c/1136x1515/vsco5d02afba4423e.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d5376942fb83812b25c4901/1133x1600/vsco5d5376af56c73.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d4c67a24cf98a42dd2fea7d/960x1434/vsco5d4c67a4c76f1.jpg',
+            'class': 'img-half'
+          },
+
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5cfcdb567ed6675ca8167d23/vsco5cfcdb594f238.jpg',
+            'class': 'img-full'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d18ecf2124a810ecf6974fb/960x1484/vsco5d18ecfaa0189.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d18d6afff20b509d56f2095/960x1435/vsco5d18d6b11353e.jpg',
+            'class': 'img-half'
+          },
+          
+         
+          
+          
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d027fec0ba2154f201d72a6/vsco5d027fef5b8b6.jpg',
+            'class': 'img-full'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d03a8dc3933431b80472806/960x1437/vsco5d03a8dec2544.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d18e974124a810ecf6974f0/960x1434/vsco5d18e976652fc.jpg',
+            'class': 'img-half'
+          },
+          {
+            'src': 'https://image-aws-us-west-2.vsco.co/93cb74/12998763/5d53764f2fb83812b25c4900/vsco5d53765ddd9c8.jpg',
+            'class': 'img-full'
+          },
+         
+        ]
+      
+    }
   }
